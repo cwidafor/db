10a2787a3062e74f8b34d2aed9c9ce566c94c892
Chris Pinchot <Chris@Chriss-MacBook-Pro.local>
2017-11-26 20:32:06 -0500

    download

---
+++ src/components/example/example.vue
@@ -0,0 +1,37 @@
+<template><div class="app-container">
+
+</div>
+</template><script>module.exports = {
+
+  mixins: [
+
+	],
+
+  data: function () {
+    return{
+      
+    }
+  },
+
+  props: [
+
+  ],
+
+  computed: {
+
+  },
+
+  methods: {
+
+
+  },
+
+  created: function(){
+    
+  },
+
+  mounted: function() {
+
+  }
+}
+</script>
